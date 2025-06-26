c22dc015ed12f751de913cdde983811d7b1ae265
SimonDemarty <simon@demarty.org>
2025-06-26 10:50:13 +0200

    started debug on generate secret key.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use num_bigint::{BigUint, RandBigInt, RandomBits};
-use num_traits::{FromPrimitive, One};
+use num_traits::{ConstZero, FromPrimitive, One};
+use num_primes::Generator;
 
 use std::fs::File;
 use std::io::{self, Write};
@@ -33,19 +34,7 @@ impl DGHV {
 
     fn generate_secret_key(&self) -> BigUint {
         // Generates the secret key
-        //      1. Generate a eta-bit integer p.
-
-        let mut rng = rand::thread_rng();
-
-        let lower_bound: BigUint = BigUint::from(2u8).pow(self.eta - 1);
-        let upper_bound: BigUint = BigUint::from(2u8).pow(self.eta);
-
-        let mut p: BigUint = rng.gen_biguint_range(&lower_bound, &upper_bound);
-
-        if &p % BigUint::from_u8(2).unwrap() == BigUint::from_u8(0).unwrap() {
-            p += BigUint::one();
-        }
-
+        let p = Generator::new_prime(self.eta as usize);
         p
     }
 
@@ -59,16 +48,84 @@ impl DGHV {
         let q_bound: BigUint = BigUint::from(2u8).pow(self.gamma - self.eta); // TODO: check if true.
         let r_bound: BigUint = BigUint::from(2u8).pow(self.rho);
 
-        for _i in 0..self.tau {
-            let q_i: BigUint = rng.gen_biguint_below(&q_bound);
-            let r_i: BigUint = rng.gen_biguint_below(&r_bound);
-            let x_i: BigUint = q_i * secret_key + r_i;
-            pk.push(x_i);
-        }
+        loop {
+             pk.clear();
 
+            let mut max: BigUint = BigUint::ZERO;
+            let mut max_i: usize = 0;
+            for i in 0..self.tau {
+                let q_i: BigUint = rng.gen_biguint_below(&q_bound);
+                let r_i: BigUint = rng.gen_biguint_below(&r_bound);
+                let x_i: BigUint = q_i * secret_key + r_i;
+    
+                pk.push(x_i.clone());
+    
+                if max < x_i {
+                    max = x_i;
+                    max_i = i as usize;
+                }
+            }
+
+            // make sure x_0 is odd
+            if &max % BigUint::from_u8(2).unwrap() == BigUint::ZERO {
+                max += BigUint::one();
+            }
+
+            // make sure [x_0]p is even
+            let x0_modp: BigUint = &max % secret_key;
+            let x0_modp_centered: BigUint;
+            if x0_modp > secret_key / &BigUint::from(2u8) {
+                x0_modp_centered = x0_modp - secret_key;
+            } else {
+                x0_modp_centered = x0_modp;
+            }
+
+            if &x0_modp_centered % BigUint::from_u8(2).unwrap() == BigUint::one() {
+                pk.swap(max_i, 0);
+                break;
+            }
+            
+        }
         pk
     }
 
+    // Encrypt & Decrypt
+    pub fn encrypt(&self, message_bit: u8) -> Option<BigUint> {
+        // Encrypts a message bit.
+        // Returns None if the message bit is invalid.
+
+        // 1. Validate the message bit.
+        if message_bit > 1 {
+            eprintln!("[ERROR]: message_bit should be 0 or 1 - not {}", message_bit);
+            return None;
+        }
+
+        let mut rng = rand::thread_rng();
+        let mut ciphertext = BigUint::ZERO;
+
+        // TODO: check if correct.
+        // Subset choice
+        for x_i in &self.public_key {
+            if rng.gen_bool(0.5) {
+                ciphertext += x_i;
+            }
+        }
+
+        // 3. Generate a small random noise 'r'.
+        // r is typically chosen from [0, 2^rho) or (-2^rho, 2^rho).
+        // For BigUint, we'll generate a non-negative r in [0, 2^rho).
+        let r_bound = BigUint::from(2u8).pow(self.rho);
+        let r = rng.gen_biguint_below(&r_bound);
+
+        // 4. Calculate the final ciphertext: c = (sum of x_i in subset) + 2*r + message_bit
+        let two = BigUint::from_u8(2).unwrap();
+        let message_as_biguint = BigUint::from_u8(message_bit).unwrap();
+
+        ciphertext += &two * r; // Add 2 * r
+        ciphertext += message_as_biguint; // Add the message bit
+
+        Some(ciphertext) // Return the calculated ciphertext
+    }
 }
 
 fn main() -> io::Result<()> {
